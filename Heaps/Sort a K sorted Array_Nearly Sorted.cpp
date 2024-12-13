fad82593956d3135c6d7d9b17cd44d7f0c4cb6e2
Ganesh Puppala <74791350+Ganesh-2003@users.noreply.github.com>
2024-12-13 21:12:31 +0530

    Commit

--- Heaps/Sort a K sorted Array_Nearly Sorted.cpp
+++ Heaps/Sort a K sorted Array_Nearly Sorted.cpp
@@ -1,10 +1,5 @@
 https://www.geeksforgeeks.org/problems/nearly-sorted-1587115620/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=practice_card
 
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
 // } Driver Code Ends
 // User function Template for C++
 class Solution {
@@ -40,32 +35,3 @@ class Solution {
     }
 };
 
-//{ Driver Code Starts.
-
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        string ks;
-        getline(cin, ks);
-        int k = stoi(ks);
-        Solution obj;
-        obj.nearlySorted(arr, k);
-        for (int i = 0; i < arr.size(); i++) {
-            cout << arr[i] << " ";
-        }
-        cout << endl;
-        // cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
