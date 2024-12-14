03db13e057af0c85023c884f65e73cfc04db34c0
Ganesh Puppala <74791350+Ganesh-2003@users.noreply.github.com>
2024-12-15 01:05:09 +0530

    Commit

---
+++ Heaps/Top K Frequent.cpp
@@ -0,0 +1,29 @@
+https://leetcode.com/problems/top-k-frequent-elements/
+
+class Solution {
+    public:
+        vector<int> topKFrequent(vector<int>& nums, int k) {
+            unordered_map<int,int> mp;
+            priority_queue<pair<int,int>> pq; //MaxHeap
+            
+            vector<int> ans;
+            for(auto i:nums)
+            {
+                mp[i]++;
+            }
+    
+            for(auto i:mp)
+            {
+                pq.push({i.second,i.first});
+            }
+    
+            for(int i = 0;i<k;i++)
+            {
+                ans.push_back(pq.top().second);
+                pq.pop();    
+            }
+    
+            return ans;
+            
+        }
+    };
