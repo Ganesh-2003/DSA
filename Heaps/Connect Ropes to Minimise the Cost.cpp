af378e7ac63e600e5fd603e8cf34247d2ced4fd9
Ganesh Puppala <74791350+Ganesh-2003@users.noreply.github.com>
2025-01-09 00:08:39 +0530

    commit

---
+++ Heaps/Connect Ropes to Minimise the Cost.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    public:
+      // Function to return the minimum cost of connecting the ropes.
+      int minCost(vector<int>& arr) {
+          // code here
+          priority_queue<int,vector<int>,greater<int>> pq; //Min Heap
+          
+          for(auto i:arr)
+          {
+              pq.push(i);
+          }
+          
+          int total = 0;
+          
+          while(pq.size()!=0)
+          {
+              int first = pq.top();
+              pq.pop();
+              int second = pq.top();
+              pq.pop();
+              
+              int cost = first + second;
+              total = total + cost;
+              
+              pq.push(cost);
+              
+          }
+          
+          return total;
+      }
+  };
