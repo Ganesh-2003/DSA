70cfd4e00af29b4c697cb33231cb90e5cf874cd1
Ganesh Puppala <74791350+Ganesh-2003@users.noreply.github.com>
2025-01-12 15:58:25 +0530

    commit

--- Arrays/Largest Element in an Array.cpp
+++ Arrays/Largest Element in an Array.cpp
@@ -1,1 +1,20 @@
+Link: https://www.geeksforgeeks.org/problems/largest-element-in-array4009/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=practice_card
 
+
+class Solution {
+    public:
+      int largest(vector<int> &arr) {
+          // code here
+          int res = INT_MIN;
+          
+          for(int i = 0;i<arr.size();i++)
+          {
+              if(arr[i]>res)
+              {
+                  res = arr[i];
+              }
+          }
+          
+          return res;
+      }
+  };
