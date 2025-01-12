0bfad806f596beaa594bd437a26cbedc0a0fed18
Ganesh Puppala <74791350+Ganesh-2003@users.noreply.github.com>
2025-01-12 18:50:10 +0530

    12th jan

---
+++ Arrays/RotateArray.cpp
@@ -0,0 +1,17 @@
+https://www.naukri.com/code360/problems/left-rotate-an-array-by-one_5026278?utm_source=striver&utm_medium=website&utm_campaign=codestudio_a_zcourse&leftPanelTabValue=SUBMISSION
+
+#include <bits/stdc++.h> 
+vector<int> rotateArray(vector<int>& arr, int n) {
+    // Write your code here.
+    vector<int> ans;
+
+    int temp = arr[0];
+
+    for(int i = 1;i<arr.size();i++)
+    {
+        ans.push_back(arr[i]);
+    }
+
+    ans.push_back(temp);
+    return ans;
+}
