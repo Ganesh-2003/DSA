79568dac19785ecd6f78e077f5babcf88f1eae0a
Ganesh Puppala <74791350+Ganesh-2003@users.noreply.github.com>
2025-01-31 23:34:37 +0530

    intial commit

---
+++ Hashmaps Swagwala/countsbuarray.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h> 
+int countSubarrays(int n, vector<int> &arr){
+    // Write your code here.
+    unordered_map<int,int> mp;
+    int prefixSum = 0;
+    int maxLen = 0;
+    int count = 0;
+    mp[0] = 1;
+
+    for(int i = 0;i<arr.size();i++)
+    {
+        prefixSum = prefixSum + arr[i];
+
+        if(mp.find(prefixSum) != mp.end())
+        {
+            count = count + mp[prefixSum];
+            mp[prefixSum]++;
+        }
+        else
+        {
+            mp[prefixSum] = 1;
+        }
+    }
+    return count;
+    
+}
---
+++ Hashmaps Swagwala/largsub0sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    int maxLen(vector<int>& arr) {
+        // code here
+        unordered_map<int,int> mp;
+        int prefixSum = 0;
+        int ans = 0;
+        mp[0] = -1;
+        
+        for(int i=0;i<arr.size();i++)
+        {
+            prefixSum = prefixSum + arr[i];
+            
+            if(mp.find(prefixSum) != mp.end())
+            {
+                ans = max(ans, i - mp[prefixSum]);
+            }
+            else
+            {
+                mp[prefixSum] = i;
+            }
+        }
+        
+        return ans;
+    }
+};
