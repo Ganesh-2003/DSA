e022b08afd8355293a54a34c319b77a32a8f4c3b
Ganesh-2003 <ganeshvsp12@gmail.com>
2025-02-11 23:26:04 +0530

    intial commit

---
+++ Hashmaps Swagwala/checkifArraypairDivisible.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    public:
+        bool canArrange(vector<int>& arr, int k) {
+    
+            unordered_map<int,int> mp;
+    
+            for(int i = 0;i<arr.size();i++)
+            {
+                int currentRem = (arr[i] % k + k)%k;
+                mp[currentRem]++;        
+            }
+    
+            for(int i=0;i<arr.size();i++)
+            {
+                int remainder = (arr[i] % k + k) % k;
+    
+                if(remainder == 0) 
+                {
+                    if(mp[remainder] % 2 != 0)
+                    {
+                        return false;
+                    }
+                }
+                else if (mp[remainder] != mp[k-remainder])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
