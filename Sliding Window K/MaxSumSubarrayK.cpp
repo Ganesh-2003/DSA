a97ff7c05d84585271ff83d6b3dd2d07f038df39
Ganesh-2003 <ganeshvsp12@gmail.com>
2025-02-14 11:00:24 +0530

    intial commit

--- Sliding Window K/MaxSumSubarrayK.cpp
+++ Sliding Window K/MaxSumSubarrayK.cpp
@@ -1,32 +1,31 @@
 https://www.geeksforgeeks.org/problems/max-sum-subarray-of-size-k5313/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=practice_card
 
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
-  public:
-    vector<int> FirstNegativeInteger(vector<int>& arr, int k) {
-        // write code here
-        int i = 0;
-        int j = 0;
-        
+public:
+    vector<int> countDistinct(vector<int> &arr, int k) {
         int n = arr.size();
-        vector<int> res;
-        queue<int> q;
-        while(j<n)
-        {
-            if(arr[j]<0)
-            {
-                q.push_back(arr[j]);
+        vector<int> ans;
+        if (k > n) return ans;
+
+        for (int i = 0; i <= n - k; i++) {
+            set<int> s;
+            for (int j = i; j < i + k; j++) {
+                s.insert(arr[j]);
             }
-            
-            if(j-i+1<k)
-            {
-                j++;
-            }
-            else if(j-i+1==k)
-            {
-                
-                i++;
-                j++;
-            }
+            ans.push_back(s.size());
         }
+        return ans;
     }
 };
+
+int main() {
+    Solution sol;
+    vector<int> arr = {1, 2, 1, 3, 4, 2, 3};
+    int k = 4;
+    vector<int> result = sol.countDistinct(arr, k);
+    for (int x : result) cout << x << " ";
+    return 0;
+}
