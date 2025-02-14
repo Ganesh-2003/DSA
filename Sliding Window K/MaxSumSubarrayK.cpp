41cad0cbfb1cbb349cb6c56311d0a15a483a0480
Ganesh-2003 <ganeshvsp12@gmail.com>
2025-02-14 11:06:24 +0530

    intial commit

--- Sliding Window K/MaxSumSubarrayK.cpp
+++ Sliding Window K/MaxSumSubarrayK.cpp
@@ -1,31 +1,36 @@
-https://www.geeksforgeeks.org/problems/max-sum-subarray-of-size-k5313/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=practice_card
+https://www.geeksforgeeks.org/problems/max-sum-subarray-of-size-k5313/1
 
-#include <bits/stdc++.h>
-using namespace std;
-
+// } Driver Code Ends
 class Solution {
-public:
-    vector<int> countDistinct(vector<int> &arr, int k) {
+  public:
+    int maximumSumSubarray(vector<int>& arr, int k) {
+        // code here
         int n = arr.size();
-        vector<int> ans;
-        if (k > n) return ans;
-
-        for (int i = 0; i <= n - k; i++) {
-            set<int> s;
-            for (int j = i; j < i + k; j++) {
-                s.insert(arr[j]);
+        int i = 0;
+        int j = 0;
+        int sum = 0;
+        int maxSum = 0;
+        
+        while(j<n)
+        {    
+            sum = sum+arr[j];
+            
+            if(j-i+1 < k)
+            {
+                j++;
             }
-            ans.push_back(s.size());
+            
+            else if(j-i+1 == k)
+            {
+                maxSum = max(maxSum,sum);
+                
+                sum = sum-arr[i];
+                i++;
+                j++;
+            }
         }
-        return ans;
+        
+        return maxSum;
     }
 };
 
-int main() {
-    Solution sol;
-    vector<int> arr = {1, 2, 1, 3, 4, 2, 3};
-    int k = 4;
-    vector<int> result = sol.countDistinct(arr, k);
-    for (int x : result) cout << x << " ";
-    return 0;
-}
