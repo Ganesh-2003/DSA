32fc53a62d51bf60d6e3083689b6311726b45034
Ganesh-2003 <ganeshvsp12@gmail.com>
2025-02-21 21:58:18 +0530

    intial commit

---
+++ Hashmaps Swagwala/28. TwoSum.cpp
@@ -0,0 +1,23 @@
+https://leetcode.com/problems/two-sum/
+
+class Solution {
+    public:
+        vector<int> twoSum(vector<int>& nums, int target) {
+            
+            unordered_map<int,int> mp;
+    
+            for(int i = 0;i<nums.size();i++)
+            {
+                int required = target - nums[i];
+    
+                if(mp.find(required) != mp.end())
+                {
+                    return {mp[required],i};
+                }
+    
+                mp[nums[i]] = i;
+            }
+    
+            return {-1,-1};
+        }
+    };
