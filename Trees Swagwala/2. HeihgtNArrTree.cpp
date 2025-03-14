6d2e9802d6fac60f0a18e5f22dac2e7057549669
Ganesh-2003 <ganeshvsp12@gmail.com>
2025-03-15 00:50:28 +0530

    intial commit

---
+++ Trees Swagwala/2. HeihgtNArrTree.cpp
@@ -0,0 +1,40 @@
+https://leetcode.com/problems/maximum-depth-of-n-ary-tree/description/
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public:
+        int maxDepth(Node* root) {
+            
+            if(root == NULL) {
+                return 0;
+            }
+            
+            int ans = 0;
+    
+            for(auto currentChild: root->children) {
+                ans = max(ans,maxDepth(currentChild));
+            }
+    
+            return 1+ans;
+    
+        }
+    };
