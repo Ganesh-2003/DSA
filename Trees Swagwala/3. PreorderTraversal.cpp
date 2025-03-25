e570de1bcbe981086e5e87ada708f6887c2abb5e
Ganesh-2003 <ganeshvsp12@gmail.com>
2025-03-25 22:32:36 +0530

    Intial commit

---
+++ Trees Swagwala/3. PreorderTraversal.cpp
@@ -0,0 +1,35 @@
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    public:
+    
+        void preorderRecursion(TreeNode* root, vector<int> &ans) {
+    
+            if(root == NULL) {
+                return ;
+            }
+    
+            ans.push_back(root->val);
+            preorderRecursion(root->left,ans);
+            preorderRecursion(root->right,ans);
+        }
+    
+        vector<int> preorderTraversal(TreeNode* root) {
+            vector<int> ans;
+    
+            preorderRecursion(root,ans);
+    
+            return ans;
+        }
+    };
